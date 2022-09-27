458a10ec5d4230ff1bf76bfb865bf2d0b8153487
Emre Can Uğurlu <emrecnugurlu@gmail.com>
2022-09-28 00:32:14 +0300

    if else kullanımı

---
+++ bolum2/if_else_kullanimi.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int sayi1 = 20;
+  int sayi2 = 20;
+
+  if (sayi1 < sayi2) {
+    print("$sayi1 küçüktür $sayi2");
+  } 
+  else if(sayi1 > sayi2){
+    print("$sayi1 büyüktür $sayi2");
+  }
+  else{
+    print("$sayi1 eşittir $sayi2");
+  }
+}
