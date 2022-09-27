c0978f0375be273a3f4c641df2bef66201ca90ed
Emre Can Uğurlu <emrecnugurlu@gmail.com>
2022-09-27 16:55:38 +0300

    null safety

---
+++ bolum1/num_boolean.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  int sayi = 10;
+  print(sayi);
+  sayi = 20;
+  print(sayi);
+
+  num yil = 2022;
+  print(yil);
+
+  double yil2 = 2000;
+  print(yil2);
+
+  String? name;
+  print(name);
+
+  int hex = 0xAABBCC;
+  print(hex);
+}
