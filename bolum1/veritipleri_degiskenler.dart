6969d9b678c25d20228b34334dbe238172744f6f
Emre Can Uğurlu <emrecnugurlu@gmail.com>
2022-09-27 15:59:27 +0300

    Dart dili değişkenleri...

--- bolum1/veritipleri_degiskenler.dart
+++ bolum1/veritipleri_degiskenler.dart
@@ -3,5 +3,15 @@ void main(List<String> args) {
   double ortalama = 5.6;
   num yil = 1998;
 
+  String ad = "Emre Can";
+
+  bool a = true;
+  bool b = false;
+
+  var name = "Ali";
+  var no = 5;
+  var f = 3.2;
+  var d = true;
+
   print(yas);
 }
